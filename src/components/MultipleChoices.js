6d51a48c0c5c636012f28de5837a683c4cfdf918
Tuan Ngo <tuannm79@gmail.com>
2017-03-25 16:56:34 +0700

    add Multiple choices question

--- src/components/MultipleChoices.js
+++ src/components/MultipleChoices.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
 
+class MultipleChoicesItem extends Component {
+  handleSelect = () => {
+    this.props.onSelect(this.props.questionId, this.props.id)
+  }
+
+  render() {
+    return <div><input
+      type="radio"
+      onClick={this.handleSelect}
+      value={this.props.id}
+      name={this.props.name}
+    />{this.props.value}</div>
+  }
+}
+
 class MultipleChoices extends Component {
   render() {
     return (
       <div>
-        MultipleChoices
+        <div>{this.props.question}</div>
+        <div>{this.props.options.map(
+          option =>
+          <MultipleChoicesItem
+            key={option.id}
+            {...option}
+            onSelect={this.props.onSelect}
+            questionId={this.props.id}
+            name={`${this.props.type}-${this.props.id}`}
+          />
+        )}
+        </div>
       </div>
     );
   }
--- src/stories/index.js
+++ src/stories/index.js
@@ -3,6 +3,27 @@ import { storiesOf, action, linkTo } from '@kadira/storybook'
 import MultipleChoices from '../components/MultipleChoices'
 
 storiesOf('Multiple Choice', module)
-  .add('', () => (
-    <MultipleChoices />
-  ));
+  .add('', () => {
+    const props = {
+      onSelect: action("onSelect"),
+      "id": 1,
+      question: "What color is the sky?",
+      options: [
+        {
+        "id": "a",
+        "value": "red"
+        },
+        {
+        "id": "b",
+        "value": "blue"
+        },
+        {
+        "id": "c",
+        "value": "green"
+        }
+      ]
+    }
+    return (
+      <MultipleChoices {...props} />
+    )
+  });
